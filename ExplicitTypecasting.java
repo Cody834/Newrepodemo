57fa2cce541b1148462f54f9b57518373f503a14
Cody834 <78084848+Cody834@users.noreply.github.com>
2021-04-19 11:01:16 +0200

    Update ExplicitTypecasting.java

--- ExplicitTypecasting.java
+++ ExplicitTypecasting.java
@@ -3,7 +3,7 @@ package program.jav.demos;
 public class ExplicitTypecasting {
 	public static void main (String args[]) {
 		short c=10;         //Size 2 
-		byte d=(byte)c;     // Size 1 so use explicit // (Type)Variablename;
+		 d=(byte)c;     // Size 1 so use explicit // (Type)Variablename;
 		System.out.println(c);
 		System.out.println(d);
 	}
